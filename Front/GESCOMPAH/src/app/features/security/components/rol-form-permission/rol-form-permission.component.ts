7ee85b91868be2b0171aa9dbfe8830e8e32095a5
JesusDavid1405 <jesusdavidfierrorivera817@gmail.com>
2025-08-10 15:55:04 -0500

    Creacion de los componentes restantes del modelo de seguridad

---
+++ Front/GESCOMPAH/src/app/features/security/components/rol-form-permission/rol-form-permission.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rol-form-permission',
+  imports: [],
+  templateUrl: './rol-form-permission.component.html',
+  styleUrl: './rol-form-permission.component.css'
+})
+export class RolFormPermissionComponent {
+
+}
