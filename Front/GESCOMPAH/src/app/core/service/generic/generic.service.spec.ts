0c510f7ba37f20192e0b0334517d7b501f9e0486
Guerrero-077 <120676109+Guerrero-077@users.noreply.github.com>
2025-08-12 13:01:05 -0500

    fix: correct getById URL and add unit test

--- Front/GESCOMPAH/src/app/core/service/generic/generic.service.spec.ts
+++ Front/GESCOMPAH/src/app/core/service/generic/generic.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { GenericService } from './generic.service';
+import { environment } from '../../../../environments/environment.development';
+
+interface DummyModel {
+  id: number;
+  name: string;
+}
 
 describe('GenericService', () => {
-  let service: GenericService;
+  let service: GenericService<DummyModel>;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GenericService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GenericService]
+    });
+
+    service = TestBed.inject(GenericService<DummyModel>);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('builds correct URL when requesting by id', () => {
+    const id = 123;
+    service.getById('Entidad', id).subscribe();
+
+    const req = httpMock.expectOne(`${environment.apiURL}/Entidad/${id}`);
+    expect(req.request.method).toBe('GET');
+    req.flush({ id, name: 'test' });
   });
 });
+
