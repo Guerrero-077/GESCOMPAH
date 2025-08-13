c96a5c3342dafffd6392611f79af963bcd49055c
JesusDavid1405 <jesusdavidfierrorivera817@gmail.com>
2025-08-13 09:34:57 -0500

    Mejorando diseño del inicio

--- Front/GESCOMPAH/src/app/features/public/pages/landing/landing.component.ts
+++ Front/GESCOMPAH/src/app/features/public/pages/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { CardFeatureComponent } from '../../components/card-feature/card-feature.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, CardFeatureComponent],
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
