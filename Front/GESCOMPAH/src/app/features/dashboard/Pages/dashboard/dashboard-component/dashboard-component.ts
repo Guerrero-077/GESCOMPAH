3a3cc70bda2bac32de4391842727048ff5087522
JesusDavid1405 <jesusdavidfierrorivera817@gmail.com>
2025-08-20 10:03:09 -0500

    Alertas del sistema, estructura html

--- Front/GESCOMPAH/src/app/features/dashboard/Pages/dashboard/dashboard-component/dashboard-component.ts
+++ Front/GESCOMPAH/src/app/features/dashboard/Pages/dashboard/dashboard-component/dashboard-component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardInfoComponent } from '../../../components/card-info/card-info.component';
+import { SystemAlertComponent } from "../../../components/system-alert/system-alert.component";
 
 @Component({
   selector: 'app-dashboard-component',
-  imports: [CommonModule, CardInfoComponent],
+  imports: [CommonModule, CardInfoComponent, SystemAlertComponent],
   templateUrl: './dashboard-component.html',
   styleUrl: './dashboard-component.css'
 })
