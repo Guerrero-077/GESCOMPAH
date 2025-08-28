f25a5f42569d8d7b349954057877374c8e61cb43
angelgarcia0 <garmiguelangel89>
2025-08-28 10:56:43 -0500

    Merge develop

--- Back/WebGESCOMPAH/Business/Interfaces/Implements/Business/IAppointmentService.cs
+++ Back/WebGESCOMPAH/Business/Interfaces/Implements/Business/IAppointmentService.cs
@@ -5,5 +5,7 @@ namespace Business.Interfaces.Implements.Business
 {
     public interface IAppointmentService : IBusiness<AppointmentSelectDto, AppointmentCreateDto, AppointmentUpdateDto>
     {
+        Task<bool> ChangesStatusAsync(int id, int status);
+        //Task<ContractCreateDto> AprovedAppointment(int id);
     }
 }
