6d8e91f2d8d1d1724396c890376090fdd55c67dd
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-09-04 07:49:22 -0500

    Fix migratios

--- Front/GESCOMPAH/src/environments/environment.development.ts
+++ Front/GESCOMPAH/src/environments/environment.development.ts
@@ -1,17 +1,4 @@
 export const environment = {
-  // apiURL: "http://localhost:8080/api"
-  // apiURL: "http://192.168.1.8:5163/api"
-  // apiURL: "http://localhost:8080/api"
 
-  apiURL: "http://localhost:8085/api"
-
-
-
-
-  // apiURL: "http://localhost:8080/api"
-  // apiURL: "http://192.168.1.8:8080/api"
-  // apiURL: "http://192.168.1.8:5163/api"
-
-  // apiURL: "http://localhost:8085/api"
-  // apiURL: "http://localhost:5163/api"
+  apiURL: "http://localhost:8080/api"
 };
--- Front/GESCOMPAH/src/environments/environment.ts
+++ Front/GESCOMPAH/src/environments/environment.ts
@@ -1,7 +1,3 @@
 export const environment = {
-  //  apiURL: "http://localhost:8080/api"
-  apiURL: "http://localhost:8085/api"
-
-
-  // apiURL: "https://localhost:7165/api"
+  apiURL: "http://localhost:8080/api"
 };
