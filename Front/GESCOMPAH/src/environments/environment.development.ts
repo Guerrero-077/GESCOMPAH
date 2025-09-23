6444330b8420d1701bce2081ee69226616df1150
JesusDavid1405 <jesusdavidfierrorivera817@gmail.com>
2025-09-23 00:01:27 -0500

    enviroment

--- Front/GESCOMPAH/src/environments/environment.development.ts
+++ Front/GESCOMPAH/src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
 
   // apiURL: "PENDIENTE"
-  apiURL: "https://7ef84bb9f402.ngrok-free.app/api",
+  apiURL: "https://6ecf35302fb9.ngrok-free.app/api",
 
   // 0 = sin limite; ajusta segun despliegue
-  apiURL: "https://7ef84bb9f402.ngrok-free.app/api",
+  // apiURL: "https://7ef84bb9f402.ngrok-free.app/api",
   establishmentsDefaultLimit: 0
   // apiURL: "https://localhost:7165/api"
 };
