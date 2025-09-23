6f2b02d05dd5702ecc63e3ed2da60341d0e0702d
JesusDavid1405 <jesusdavidfierrorivera817@gmail.com>
2025-09-22 21:19:02 -0500

    Environment con direccion web del servidor

--- Front/GESCOMPAH/src/environments/environment.development.ts
+++ Front/GESCOMPAH/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
   // apiURL: "PENDIENTE"
-  apiURL: "https://localhost:7165/api"
-
+  // apiURL: "https://localhost:7165/api"
+  apiURL: "https://7ef84bb9f402.ngrok-free.app/api"
 };
--- Front/GESCOMPAH/src/environments/environment.ts
+++ Front/GESCOMPAH/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   // apiURL: "http://localhost:8085/api"
-  apiURL: "https://localhost:7165/api"
+  // apiURL: "https://localhost:7165/api"
+  apiURL: "https://7ef84bb9f402.ngrok-free.app/api"
 };
