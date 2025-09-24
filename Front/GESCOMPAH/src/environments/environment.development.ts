6f947cc7347ca1b727b9bacf3c5d8b56436942fb
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-09-24 13:05:18 -0500

    Cambios nombre header

--- Front/GESCOMPAH/src/environments/environment.development.ts
+++ Front/GESCOMPAH/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
 
-  apiURL: "https://cb6c1ad656cb.ngrok-free.app/api",
+  apiURL: "https://ae377c61b925.ngrok-free.app/api",
 
   // 0 = sin limite; ajusta segun despliegue
   establishmentsDefaultLimit: 0
--- Front/GESCOMPAH/src/environments/environment.ts
+++ Front/GESCOMPAH/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: "https://cb6c1ad656cb.ngrok-free.app/api",
+  apiURL: "https://ae377c61b925.ngrok-free.app/api",
   // 0 = sin limite; ajusta segun despliegue
   establishmentsDefaultLimit: 0
 };
